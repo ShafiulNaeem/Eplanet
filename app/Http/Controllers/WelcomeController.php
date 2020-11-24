c78b6eb37796912322c15533251397231fc7fffc
tushar <tushar.khan0122@gmail.com>
2020-11-25 03:57:29 +0600

    add category in product table and factory file added

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -14,46 +14,8 @@ class WelcomeController extends Controller
     public function index(){
 
         // Category wise Product
-        $catArray = ['kids', 'men', 'women', 'electronics'];
-        $mainRes = [];
+        $mainRes = $this->productByCategory(['kids', 'men', 'women', 'electronics']);
 
-        foreach ($catArray as $val){
-            $category_result = [];
-            $subCategories = Category::with('subcategory')->where([
-                ['category_name' , '=', $val],
-                ['status', '=', 1],
-            ])->get();
-
-
-            foreach ($subCategories as $sub){
-
-                $category_result['category'] = [
-                    'category_name' => $sub->category_name,
-                    'category_image' => $sub->category_image
-                ];
-
-                foreach ($sub->subcategory as $subCat){
-                $product = SubCategory::with('products')->where('id', $subCat->id)->get();
-                    if (count($product) > 0){
-                        foreach ($product as $pval) {
-                            foreach ($pval->products as $products){
-                            $category_result['category']['products'][] = [
-                                'id' => $products->id,
-                                'unique_id' => $products->unique_id,
-                                'brand_id' => $products->brand_id,
-                                'product_name' => $products->product_name,
-                                'feature_image' => $products->feature_image,
-                                'stock' => $products->stock,
-                                'product_price' => $products->product_price
-                            ];
-
-                            }
-                        }
-                    }
-                }
-                $mainRes[]=$category_result;
-            }
-        }
 
         //Category Query
         $category = Category::orderBy('category_name','asc')->get();
@@ -128,5 +90,45 @@ class WelcomeController extends Controller
         return view('pages.categories',['categories' =>$category]);
     }
 
+    private function productByCategory($catArray){
+        $mainRes = [];
+        foreach ($catArray as $val){
+            $category_result = [];
+            $subCategories = Category::with('subcategory')->where([
+                ['category_name' , '=', $val],
+                ['status', '=', 1],
+            ])->get();
 
+
+            foreach ($subCategories as $sub){
+
+                $category_result['category'] = [
+                    'category_name' => $sub->category_name,
+                    'category_image' => $sub->category_image
+                ];
+
+                foreach ($sub->subcategory as $subCat){
+                    $product = SubCategory::with('products')->where('id', $subCat->id)->get();
+                    if (count($product) > 0){
+                        foreach ($product as $pval) {
+                            foreach ($pval->products as $products){
+                                $category_result['category']['products'][] = [
+                                    'id' => $products->id,
+                                    'unique_id' => $products->unique_id,
+                                    'brand_id' => $products->brand_id,
+                                    'product_name' => $products->product_name,
+                                    'feature_image' => $products->feature_image,
+                                    'stock' => $products->stock,
+                                    'product_price' => $products->product_price
+                                ];
+
+                            }
+                        }
+                    }
+                }
+                $mainRes[]=$category_result;
+            }
+        }
+        return $mainRes;
+    }
 }
