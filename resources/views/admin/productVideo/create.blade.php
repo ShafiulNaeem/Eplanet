ed336b6edcda6c5c5dda5848123ffe40424e263c
tushar <tushar.khan0122@gmail.com>
2020-11-30 19:53:03 +0600

    admin changed

--- resources/views/admin/productVideo/create.blade.php
+++ resources/views/admin/productVideo/create.blade.php
@@ -2,54 +2,64 @@
 
 @section('content')
     <div class="wrapper row-offcanvas row-offcanvas-left">
-        <!-- Left side column. contains the logo and sidebar -->
-    @include('layouts.admin_sidebar')
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+        @include('layouts.admin_blade_title', [
+                'title' => 'Create Product Video'
+            ])
 
-    <!-- Right side column. Contains the navbar and content of the page -->
-        <aside class="right-side">
-
-
-            <!-- Main content -->
+        <!-- Main content -->
             <section class="content">
-
-                <div class="row">
-                    <div class="col-md-12">
-                        <!-- general form elements -->
-                        <div class="box box-primary">
-                            <div class="box-header">
-                                <h3 class="box-title">Add Product Video</h3>
-                            </div><!-- /.box-header -->
-                            <!-- form start -->
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- left column -->
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="card card-dark">
+                                <div class="card-header">
+                                    <h3 class="card-title">Upload Product Video</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <!-- form start -->
                             <form role="form" action="{{route('productVideo.store')}}" enctype="multipart/form-data" method="POST">
                                 @csrf
-                                <div class="box-body">
+                                <div class="card-body">
 
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6 float-left">
                                         <label for="exampleInputPassword1">Product Name</label>
-                                        <select name="product_name" id="" class="form-control">
+                                        <select name="product_name" id="" id="" class="form-control select2 select2-danger" data-dropdown-css-class="select2-danger" style="width: 100%;">
+                                            <option selected>Select Product</option>
                                             @foreach($products as $product)
                                                 <option value="{{$product->id}}">{{$product->product_name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
 
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6 float-left">
                                         <label for="exampleFormControlFile1">Product Video</label>
-                                        <input type="file" name="product_image" class="form-control-file" id="exampleFormControlFile1">
+{{--                                        <input type="file" name="product_image" class="form-control-file" id="exampleFormControlFile1">--}}
+                                        <div class="input-group ">
+                                            <div class="custom-file">
+                                                <input type="file" name="product_image"  class="custom-file-input" id="exampleInputFile">
+                                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                            </div>
+                                        </div>
                                     </div>
 
                                 </div><!-- /.box-body -->
 
-                                <div class="box-footer">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-outline-dark btn-block">Submit</button>
                                 </div>
                             </form>
-                        </div><!-- /.box -->
+                                <!-- /.card -->
 
-                    </div>
+                            </div>
+                            <!--/.col (left) -->
+                        </div>
+                        <!-- /.row -->
+                    </div><!-- /.container-fluid -->
                 </div>
-
-            </section><!-- /.content -->
-        </aside><!-- /.right-side -->
-    </div><!-- ./wrapper -->
+            </section>
+            <!-- /.content -->
 @endsection
--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin_blade_title')
 
 @section('content')
 <div class="container">
