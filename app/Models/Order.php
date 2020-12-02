148723275f766dffd6a5acc03cfa6a038397f645
shafi <shafiul15-1052@diu.edu.bd>
2020-12-03 02:35:13 +0600

    show order in admin panel

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $table = 'orders';
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Models\Product','order_products','order_id','product_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
