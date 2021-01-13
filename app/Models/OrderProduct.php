9bfab350c5540c332d16491e99094684a00e696a
tushar <tushar.khan0122@gmail.com>
2021-01-14 00:19:43 +0600

    admin sell report

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -10,4 +10,25 @@ class OrderProduct extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
