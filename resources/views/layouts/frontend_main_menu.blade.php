646416397a0796988c39ccd4b206439cc7146b22
tushar <tushar.khan0122@gmail.com>
2021-01-20 23:38:41 +0600

    some changes

--- resources/views/layouts/frontend_main_menu.blade.php
+++ resources/views/layouts/frontend_main_menu.blade.php
@@ -104,10 +104,11 @@
                                 </li>
                             @endif
 
-                            <li class="menu-item-has-children "><a href="{{ route('blog.allBog') }}"> Blog </a>
+                            <li class="menu-item-has-children "><a href="{{ route('blog.allBog') }}"> Vblog </a>
                             <li class="menu-item-has-children "><a href="{{route('contact.show')}}"> Contact Us</a>
                             @if( \Illuminate\Support\Facades\Auth::check())
                                 <li class="menu-item-has-children "><a href="{{route('profile.show')}}">Profile</a>
+                                <li class="menu-item-has-children "><a href="{{route('wish.list')}}">Wishlist</a>
                             @endif
 {{--                            <li class="menu-item-has-children">--}}
 {{--                                <a href="#">pages </a>--}}
@@ -320,7 +321,7 @@
         <div class="main-nav sticky-header ">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-2 col-md-2 col-2">
+                    <div class="col-lg-1 col-md-1 col-1">
                         <div class="location" style="text-align: center;margin-top:10px;">
                             <a href="{{ route('contact.show') }}"><i class="fa fa-map-marker" aria-hidden="true"></i></a>
                         </div>
@@ -373,10 +374,11 @@
                                     @endforeach
                                 @endif
 
-                                <li class="has-child c-1 c-1"><a href="{{ route('blog.allBog') }}"> Blog </a> </li>
+                                <li class="has-child c-1 c-1"><a href="{{ route('blog.allBog') }}"> Vblog </a> </li>
                                 <li class="has-child c-1"><a href="{{route('contact.show')}}"> Contact Us</a> </li>
                                 @if( \Illuminate\Support\Facades\Auth::check())
                                     <li class="has-child c-1"><a href="{{route('profile.show')}}">Profile</a> </li>
+                                    <li class="menu-item-has-children "><a href="{{route('wish.list')}}">Wishlist</a>
                                 @endif
 
 {{--                                <li class="has-child c-1">--}}
@@ -402,6 +404,7 @@
                             </ul>
                         </nav>
                     </div>
+{{--                    <div class="col-md-1"></div>--}}
                 </div>
 
             </div>
