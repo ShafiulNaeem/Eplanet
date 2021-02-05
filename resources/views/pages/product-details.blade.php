52a67d86b489e1bbeea70de9e800abea07eee20e
Yeasin123 <yeasin15-1037@diu.edu.bd>
2021-02-05 11:07:11 +0600

    ckeditor and product page modify

--- resources/views/pages/product-details.blade.php
+++ resources/views/pages/product-details.blade.php
@@ -178,6 +178,125 @@
     </div>
     <!--product details end-->
 
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-9 col-md-9">
+            <div class="product_d_inner">   
+                <div class="product_info_button">    
+                    <ul class="nav" role="tablist">
+                        <li>
+                            <a class="active" data-toggle="tab" href="#sheet" role="tab" aria-controls="sheet" aria-selected="false">Specification</a>
+                       </li>
+                        <li>
+                            <a data-toggle="tab" href="#info" role="tab" aria-controls="info" aria-selected="false">Description</a>
+                        </li>
+
+                        <li>
+                           <a data-toggle="tab" href="#reviews" role="tab" aria-controls="reviews" aria-selected="false">Reviews (1)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="tab-content">
+                   
+                    <div class="tab-pane fade active" id="sheet" role="tabpanel">
+                        <div class="product_d_table">
+                           <form action="#">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td class="first_child">Compositions</td>
+                                            <td>Polyester</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="first_child">Styles</td>
+                                            <td>Girly</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="first_child">Properties</td>
+                                            <td>Short Dress</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </form>
+                        </div>
+                        <div class="product_info_content">
+                            <p>Fashion has been creating well-designed collections since 2010. The brand offers feminine designs delivering stylish separates and statement dresses which have since evolved into a full ready-to-wear collection in which every item is a vital part of a woman's wardrobe. The result? Cool, easy, chic looks with youthful elegance and unmistakable signature style. All the beautiful pieces are made in Italy and manufactured with the greatest attention. Now Fashion extends to a range of accessories including shoes, hats, belts and more!</p>
+                        </div>    
+                    </div>
+
+                    <div class="tab-pane fade show " id="info" role="tabpanel">
+                        <div class="product_info_content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla augue nec est tristique auctor. Donec non est at libero vulputate rutrum. Morbi ornare lectus quis justo gravida semper. Nulla tellus mi, vulputate adipiscing cursus eu, suscipit id nulla.</p>
+                            <p>Pellentesque aliquet, sem eget laoreet ultrices, ipsum metus feugiat sem, quis fermentum turpis eros eget velit. Donec ac tempus ante. Fusce ultricies massa massa. Fusce aliquam, purus eget sagittis vulputate, sapien libero hendrerit est, sed commodo augue nisi non neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi. Cras neque metus, consequat et blandit et, luctus a nunc. Etiam gravida vehicula tellus, in imperdiet ligula euismod eget.</p>
+                        </div>    
+                    </div>
+
+                    <div class="tab-pane fade" id="reviews" role="tabpanel">
+                        <div class="reviews_wrapper">
+                            <div class="product_review_form">
+                                <form action="#">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <label for="review_comment">Your review </label>
+                                            <textarea name="comment" id="review_comment"></textarea>
+                                        </div> 
+                                        <div class="col-lg-6 col-md-6">
+                                            <label for="author">Name</label>
+                                            <input id="author" type="text">
+
+                                        </div> 
+                                        <div class="col-lg-6 col-md-6">
+                                            <label for="email">Email </label>
+                                            <input id="email" type="text">
+                                        </div>  
+                                    </div>
+                                    <button type="submit">Submit</button>
+                                 </form>   
+                            </div> 
+                        </div>     
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-lg-3 col-md-3">
+            <div class="row">
+                <div class="col-md-12 no-gutter">
+                    <h4 class="text-center" style="background:#D6D6D6;padding:5px 0">Related Product</h4>
+                </div>
+            </div>
+            <div class="row">
+               <div class="related_pro">
+                <div class="col-md-4">
+                    <div class="product_img">
+                        <img src="{{ asset('frontend/assets/img/artical1.jpg') }}" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="product_desc">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus soluta quas nobis recusandae distinctio
+                    </div>
+                </div>
+               </div>
+               <div class="related_pro">
+                <div class="col-md-4">
+                    <div class="product_img">
+                        <img src="{{ asset('frontend/assets/img/artical1.jpg') }}" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="product_desc">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus soluta quas nobis recusandae distinctio
+                    </div>
+                </div>
+               </div>
+             </div>
+            </div>
+        </div>
+    </div>
+</div>
+
     <!--product info start-->
     <div class="product_d_info mb-65">
 {{--        <div class="container">--}}
